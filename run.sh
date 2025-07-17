6cb22d82cd2e45266a6982eefd9414a3a5e756fe
Andrew Baumbach <andrewb@twingate.com>
2025-07-17 16:43:01 -0400

    feat: multi arch supported home assistant add on

---
+++ run.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+set -e
+
+CONFIG="/etc/twingate/connector.conf"
+CONFIG_DIR=$(dirname "$CONFIG")
+
+apt_require_curl() {
+    if ! command -v curl; then
+        apt install -yq curl
+        trap "apt purge -yq curl" EXIT
+    fi
+}
+
+apt_require_gpg() {
+    if ! command -v gpg; then
+        apt install -yq gpg
+        trap "apt purge -yq gpg" EXIT
+    fi
+}
+
+exit_no_changes () {
+    echo "Canceling setup and exiting. No changes have been made."
+    exit 1
+}
+
+if [ -f "$CONFIG" ]; then
+    echo "Config file \"${CONFIG}\" already exists"
+    exit_no_changes
+else
+    install -d -m 0700 "$CONFIG_DIR"
+    [ -f "$CONFIG" ] && mv "$CONFIG" "$CONFIG.$(date +%s)"
+fi
+
+apt update -yq
+apt_require_curl
+apt_require_gpg
+
+TWINGATE_GPG_PUBLIC_KEY=/usr/share/keyrings/twingate-client-keyring.gpg
+if ! curl -fsSL "https://packages.twingate.com/apt/gpg.key" | gpg --dearmor -o "$TWINGATE_GPG_PUBLIC_KEY"; then
+    echo "Failed to download or process GPG key"
+    exit 1
+fi
+echo "deb [signed-by=${TWINGATE_GPG_PUBLIC_KEY}] https://packages.twingate.com/apt/ /" | tee /etc/apt/sources.list.d/twingate.list
+apt update -yq
+apt install -yq twingate-connector
+
+TWINGATE_ACCESS_TOKEN=$(bashio::config 'access_token')
+if [[ -z "${TWINGATE_ACCESS_TOKEN}" ]]; then
+    echo "access_token is not set. Please set it in the configuration."
+    exit_no_changes
+fi
+export TWINGATE_ACCESS_TOKEN
+
+TWINGATE_REFRESH_TOKEN=$(bashio::config 'refresh_token')
+if [[ -z "${TWINGATE_REFRESH_TOKEN}" ]]; then
+    echo "refresh_token is not set. Please set it in the configuration."
+    exit_no_changes
+fi
+export TWINGATE_REFRESH_TOKEN
+
+TWINGATE_NETWORK=$(bashio::config 'network')
+if [[ -z "${TWINGATE_NETWORK}" ]]; then
+    echo "network is not set. Please set it in the configuration."
+    exit_no_changes
+fi
+export TWINGATE_NETWORK
+
+if [ -n "${TWINGATE_NETWORK}" ]; then
+  echo "TWINGATE_NETWORK=${TWINGATE_NETWORK}" >> "${CONFIG}"
+fi
+
+if [ -n "${TWINGATE_ACCESS_TOKEN}" ] && [ -n "${TWINGATE_REFRESH_TOKEN}" ]; then
+    { \
+        echo "TWINGATE_ACCESS_TOKEN=${TWINGATE_ACCESS_TOKEN}"; \
+        echo "TWINGATE_REFRESH_TOKEN=${TWINGATE_REFRESH_TOKEN}"; \
+    } >> "${CONFIG}"
+    chmod 0600 "$CONFIG"
+fi
+
+/usr/bin/twingate-connector
